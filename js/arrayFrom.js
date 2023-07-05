8caad4e522e8d42a49d428dc7b2e4167528d4a2c
AbhishekBhure <abhiab7411@gmail.com>
2023-07-05 07:14:08 -0700

    Array.from()

---
+++ js/arrayFrom.js
@@ -0,0 +1,17 @@
+const iLoveIceCream = (flavour) =>
+  Array.from(flavour, (ele) => `I like ${ele} ice cream`);
+
+const randomNums = (count) =>
+  Array.from(new Array(count), (ele) => parseInt(Math.random() * 10000));
+
+const genStars = (count) => Array.from(Array(count), () => "*");
+
+for (let i = 1; i <= 6; i++) {
+  console.log(genStars(i));
+}
+
+for (let i = 6; i >= 1; i--) {
+  console.log(genStars(i));
+}
+// console.log(randomNums(10));
+// console.log(iLoveIceCream(["Mango", "Banana", "Chocolate"]));
