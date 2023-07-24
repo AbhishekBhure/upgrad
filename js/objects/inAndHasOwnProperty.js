10b739034103044b7e194658f4cf0f3a6215f292
AbhishekBhure <abhiab7411@gmail.com>
2023-07-24 20:54:03 +0530

    in and hasOwnProperty changes

--- js/objects/inAndHasOwnProperty.js
+++ js/objects/inAndHasOwnProperty.js
@@ -21,5 +21,41 @@ const showDetails = (book) =>
     ? console.log(book.getSummary())
     : console.log("Unable to show details");
 
-showDetails(book1);
-showDetails(book2);
+// showDetails(book1);
+// showDetails(book2);
+
+("use strict");
+
+const rectangles = [
+  {
+    id: "Rectangle - 01",
+    width: 129.2,
+    height: 80.9,
+  },
+  {
+    id: "Rectangle - 02",
+    width: 342,
+    height: 120,
+    area() {
+      return this.width * this.height;
+    },
+  },
+];
+
+// Your Code Goes Here
+
+const calcArea = function () {
+  return this.width * this.height;
+};
+
+try {
+  rectangles.forEach((rect) =>
+    console.log(
+      `${rect.id} : ${
+        rect.hasOwnProperty("area") ? rect.area() : calcArea.call(rect)
+      }`
+    )
+  );
+} catch (error) {
+  console.log(error.message);
+}
