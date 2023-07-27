a0d36cc41e6b7b14d81e56c98df30fdbaf4f11b5
AbhishekBhure <abhiab7411@gmail.com>
2023-07-27 18:55:32 +0530

    slice() and substring() changes

--- js/strings/sliceAndSubstring.js
+++ js/strings/sliceAndSubstring.js
@@ -8,4 +8,20 @@ const age2 = str.substring(str.search("Age"), 0);
 const message = "my regsiter number is RKQ23345-22. please update";
 const pattern = message.search(/RKQ\d+\-\d+/g);
 const regNum = message.slice(pattern, pattern + 12);
-console.log(`Reg Num: ${regNum} `);
+// console.log(`Reg Num: ${regNum} `);
+
+const str1 = "name:James Dean,age:45,profession:Actor,location:Hollywood";
+
+const fullName = str1.substring(
+  str1.search("name") + 5,
+  str1.search("age") - 1
+);
+
+const profession = str1.substring(
+  str1.search("profession") + 11,
+  str1.search("location") - 1
+);
+
+const hollywood = str1.substring(str1.search("location") + 9, str1.length);
+
+console.log(`${fullName} was an ${profession} in a ${hollywood} movie`);
