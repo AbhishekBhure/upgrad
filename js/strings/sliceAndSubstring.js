74b8b86a1a76cccc85cfadc0b5e6da454f13aa1b
AbhishekBhure <abhiab7411@gmail.com>
2023-07-27 19:38:45 +0530

    trim(), trimStart() and trimEnd()

--- js/strings/sliceAndSubstring.js
+++ js/strings/sliceAndSubstring.js
@@ -1,9 +1,9 @@
 const str = "Name: Abhi, Age: 22, Profession: Full Stack";
 const age = str.slice(str.search("Age"), str.search("Age") + 7);
-const age2 = str.substring(str.search("Age"), 0);
+const age2 = str.substring(str.search("Age") - 2, 0);
 
 // console.log(age);
-// console.log(age2);
+console.log(age2);
 
 const message = "my regsiter number is RKQ23345-22. please update";
 const pattern = message.search(/RKQ\d+\-\d+/g);
---
+++ js/strings/trim.js
@@ -0,0 +1,5 @@
+const message = "           Hi hello hw are you am fine thank you          ";
+
+const result = message.trimStart();
+
+console.log(result);
