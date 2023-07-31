035196165e10f711212ed9087280397773582936
AbhishekBhure <abhiab7411@gmail.com>
2023-07-31 19:53:02 +0530

    transfered hackerrank folder to 100days-DSA

--- js/hackerRank/interview.js
+++
@@ -1,5 +0,0 @@
-console.log([1, 2] + [3, 4]);
-console.log([] + {});
-console.log(3 * "4" + 5);
-console.log(typeof NaN === "number");
-console.log(3 + +"3");
--- js/hackerRank/uniqueVal.js
+++
@@ -1,7 +0,0 @@
-const a = [1, 2, 3, 4, 3, 2, 1];
-
-function lonelyInteger(arr) {
-  return arr.find((n) => arr.indexOf(n) === arr.lastIndexOf(n));
-}
-
-console.log(lonelyInteger(a));
