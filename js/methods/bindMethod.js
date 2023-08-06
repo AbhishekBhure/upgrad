98ef135bcd89d5f774e2c7ab514e0afa24bd90b1
AbhishekBhure <abhiab7411@gmail.com>
2023-08-06 22:06:19 +0530

    methos call(), apply() and bind()

---
+++ js/methods/bindMethod.js
@@ -0,0 +1,26 @@
+let order1 = {
+  id: 1,
+  name: "John",
+  product: "fan",
+  price: 1200,
+};
+let order2 = {
+  id: 2,
+  name: "Jenny",
+  product: "cooler",
+  price: 1500,
+};
+let order3 = {
+  id: 3,
+  name: "Jen",
+  product: "ac",
+  price: 2000,
+};
+
+const showDetails = function (username) {
+  console.log(
+    `Hi ${this.id}:${this.name}, your order for ${this.product} has been placed successfully.`
+  );
+};
+
+showDetails.call(order2, ["John"]);
