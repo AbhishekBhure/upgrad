41aa0c67b033a89235f3ec26592bb6a0852a7a18
AbhishekBhure <abhiab7411@gmail.com>
2023-09-19 13:29:12 +0530

    modified class-components

--- react/class-Components/src/App.jsx
+++ react/class-Components/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Timer from "./components/Timer";
 import Child from "./components/Child";
+import Input from "./components/Input";
 
 class App extends React.Component {
   constructor(props) {
@@ -48,6 +49,16 @@ class App extends React.Component {
             {this.state.showTime ? "Hide" : "Show"}
           </button>
           {this.state.showTime && <Timer toggle={this.state.showTime} />}
+        </div>
+        <div>
+          <Input
+            render={(value) => <b className="text-white ml-5 pt-4">{value}</b>}
+          />
+          <Input
+            render={(value) => (
+              <p className="text-white ml-5 pt-4">{value.toUpperCase()}</p>
+            )}
+          />
         </div>
       </>
     );
