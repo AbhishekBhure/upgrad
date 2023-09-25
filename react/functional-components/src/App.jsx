cefa062c4b9494df75e93c0bd854606f0f3c3f60
AbhishekBhure <abhiab7411@gmail.com>
2023-09-25 12:05:49 +0530

    working  with functional-components modified App.js

--- react/functional-components/src/App.jsx
+++ react/functional-components/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 import "./App.css";
+
+const NameContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   const decrement = () => {
     if (count > 0) {
       setCount(count - 1);
     }
-  };     
+  };
+
   return (
-    <>
+    <NameContext.Provider value={name}>
       <div>
         {/* <h1>HI</h1> */}
         <button onClick={decrement}> - </button>
@@ -22,6 +26,56 @@ function App() {
         >
           +
         </button>
+        <br />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <ComponentA />
+      </div>
+    </NameContext.Provider>
+  );
+}
+
+function ComponentA() {
+  return (
+    <>
+      <div>
+        <h1>Component A</h1>
+        <ComponentB />
+      </div>
+    </>
+  );
+}
+function ComponentB() {
+  return (
+    <>
+      <div>
+        <h1>Component B</h1>
+        <ComponentC />
+      </div>
+    </>
+  );
+}
+function ComponentC() {
+  return (
+    <>
+      <div>
+        <h1>Component C</h1>
+        <ComponentD />
+      </div>
+    </>
+  );
+}
+function ComponentD() {
+  const name = useContext(NameContext);
+  return (
+    <>
+      <div>
+        <h1>{`Component D ${name}`}</h1>
       </div>
     </>
   );
