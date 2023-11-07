77b4223c15124e132c812d4cff57128323561564
AbhishekBhure <abhiab7411@gmail.com>
2023-11-07 18:23:36 +0530

    modified app.js

--- express/pug/blog-chef/app.js
+++ express/pug/blog-chef/app.js
@@ -13,7 +13,25 @@ app
   .get("/admin/login", (req, res) => res.render("login"))
   .post("/admin/login", (req, res) => res.send("<h1>Login API</h1>"));
 
-app.get("/admin/dashboard", (req, res) => res.render("dashboard"));
+app.get("/admin/dashboard", (req, res) =>
+  res.render("dashboard", {
+    username: "Joe Don",
+    posts: [
+      {
+        id: 1,
+        author: "Joe",
+        title: "Don Bolte",
+        content: "Kuch nai don ban ke raho",
+      },
+      {
+        id: 2,
+        author: "Joe Brother",
+        title: "Don Bolte Joe Bhai ko",
+        content: "Kuch nai don ban ke raho Joe Bhai",
+      },
+    ],
+  })
+);
 app.get("/admin/logout", (req, res) => res.redirect("/admin/login"));
 
 app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"));
