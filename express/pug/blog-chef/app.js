31c1ac998044e94d12ce771266967da961bf430c
AbhishekBhure <abhiab7411@gmail.com>
2023-11-07 14:45:35 +0530

    modified app.js added route and to redirect once loggedIn

--- express/pug/blog-chef/app.js
+++ express/pug/blog-chef/app.js
@@ -13,4 +13,7 @@ app
   .get("/admin/login", (req, res) => res.render("login"))
   .post("/admin/login", (req, res) => res.send("<h1>Login API</h1>"));
 
+app.get("/admin/dashboard", (req, res) => res.render("dashboard"));
+app.get("/admin/logout", (req, res) => res.redirect("/admin/login"));
+
 app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"));
