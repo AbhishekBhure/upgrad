0fde2dac09baee3bb3cfd9469a689251fa7ebb84
AbhishekBhure <abhiab7411@gmail.com>
2023-11-09 22:20:25 +0530

    modified app.js

--- express/pug/blog-chef/app.js
+++ express/pug/blog-chef/app.js
@@ -4,7 +4,10 @@ import morgan from "morgan";
 import session from "express-session";
 import { createWriteStream } from "fs";
 import compression from "compression";
-import { protectRoute } from "./utils/protectedRoute";
+import home from "./routes/home";
+import admin from "./routes/admin";
+import api from "./routes/api";
+
 const app = express();
 const logFile = join(__dirname, "blogchef.log");
 
@@ -36,56 +39,10 @@ app.use(
     },
   })
 );
+
 app.set("view engine", "pug");
-
-app.get("/", (req, res) => {
-  res.send("<h1>BlogChef</h1>");
-});
-
-app.get("/admin", (req, res) =>
-  req.session.user
-    ? res.redirect("/admin/dashboard")
-    : res.redirect("/admin/login")
-);
-
-app
-  .get("/admin/login", (req, res) => res.render("login"))
-  .post("/admin/login", (req, res) => {
-    const { email, password } = req.body;
-    if (email === "test@gmail.com" && password === "123456789") {
-      req.session.user = "Abhi";
-      return res.redirect("/admin/dashboard");
-    }
-    res.redirect("/admin/login");
-  });
-
-app.get("/admin/dashboard", protectRoute("/admin/login"), (req, res) =>
-  res.render("dashboard", {
-    username: req.session.user,
-    posts: [
-      {
-        id: 1,
-        author: "Joe",
-        title: "Don Bolte",
-        content: "Kuch nai don ban ke raho",
-      },
-      {
-        id: 2,
-        author: "Joe Brother",
-        title: "Don Bolte Joe Bhai ko",
-        content: "Kuch nai don ban ke raho Joe Bhai",
-      },
-    ],
-  })
-);
-app.get("/admin/logout", (req, res) => {
-  delete req.session.user;
-  res.redirect("/admin/login");
-});
-
-app.post("/api/posts", (res, req) => {
-  console.log(req.body);
-  res.json({ message: "Got it!" });
-});
+app.use("/", home);
+app.use("/admin", admin);
+app.use("/api", api);
 
 app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"));
