be8d965476f7641f19d731bb195bc4e2fb257e69
AbhishekBhure <abhiab7411@gmail.com>
2023-11-15 22:55:15 +0530

    modified store-post.js

--- express/pug/blog-chef/routes/api/store-post.js
+++ express/pug/blog-chef/routes/api/store-post.js
@@ -1,8 +1,10 @@
+import { deleteCache } from "../../controllers/cache";
 import { createPost } from "../../controllers/post";
 
 export default async (req, res) => {
   try {
     const post = await createPost(req.body.post);
+    deleteCache("all-posts");
     res.json({ post });
   } catch (error) {
     res.status(401).json({ error });
