df8efe9dae5cbb7f947692a90c0e61d328b610fd
AbhishekBhure <abhiab7411@gmail.com>
2023-11-15 22:53:04 +0530

    modified delete-post.js added redis controllers

--- express/pug/blog-chef/routes/api/delete-post.js
+++ express/pug/blog-chef/routes/api/delete-post.js
@@ -1,9 +1,11 @@
+import { deleteCache } from "../../controllers/cache";
 import { deletePost } from "../../controllers/post";
 
 export default async (req, res) => {
   try {
     const postId = req.params.postId;
     await deletePost(postId);
+    deleteCache([`post:${postId}`, "all-posts"]);
     res.json({ status: true });
   } catch (error) {
     res.status(401).json({ error });
