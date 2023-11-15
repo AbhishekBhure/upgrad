630d29723a08e0a092f38e3f8b53838c69767e6c
AbhishekBhure <abhiab7411@gmail.com>
2023-11-15 22:54:14 +0530

    modified get-posts.js added redis controllers

--- express/pug/blog-chef/routes/api/get-posts.js
+++ express/pug/blog-chef/routes/api/get-posts.js
@@ -1,8 +1,10 @@
+import { cacheContent } from "../../controllers/cache";
 import { getAllPosts } from "../../controllers/post";
 
 export default async (req, res) => {
   try {
     const posts = await getAllPosts();
+    cacheContent("all-posts", posts);
     res.json({ posts });
   } catch (error) {
     res.status(403).json(error);
